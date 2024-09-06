c400ffd71de17d80c4cfd9e132d0a625712f6f7e
Clemens Ager <clemens.ager@uibk.ac.at>
2024-09-06 11:28:06 +0200

    header/footer

--- super-galaxy/src/consts.ts
+++ super-galaxy/src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Clemens Blog';
+export const SITE_DESCRIPTION = 'Welcome to my personal blog!';
+export const MASTODON = 'https://social.uibk.ac.at/@ClemensAger';
+export const GHUB = 'https://github.com/bdcaf';
